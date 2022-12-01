16db9552193672c81b3d035c143359d6d5cc3a7e
nona? <79404245+not-nonano@users.noreply.github.com>
2022-12-01 12:48:39 +0800

    reset daily

--- index.js
+++ index.js
@@ -34,5 +34,46 @@ client.aliases = new Collection();
   require(`./handlers/${handler}`)(client);
 });
 
+const sql = require('mssql')
+const sqlConfig = {
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    server: process.env.DATABASE_SERVER,
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: true, // for azure
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+}
+
 
 client.login(process.env.DISCORD_TOKEN);
+
+var reset = resettimer();
+
+// if 7 then reset
+function resettimer() {
+  var h, m, s, day, month;
+  setInterval(function () {
+    var estTime = new Date();
+    var currentDateTimeCentralTimeZone = new Date(estTime.toLocaleString('en-US', { timeZone: 'Asia/Manila' }));
+    s = currentDateTimeCentralTimeZone.getSeconds();
+    m = currentDateTimeCentralTimeZone.getMinutes();
+    h = currentDateTimeCentralTimeZone.getHours();
+  }, 1000);
+
+
+
+  setInterval(async function () {
+    //console.log(h + "" + m + "" + s);
+    if (h == 7 && m == 0 && s == 0) {
+      await sql.connect(sqlConfig)
+      await sql.query(`UPDATE Economy SET daily = ${parseInt(0)} WHERE daily = ${parseInt(1)}`)
+    }
+  }, 1000);
+}
