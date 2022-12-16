be196572c3843dfe9e02609640c58a9cd248120d
nona? <79404245+not-nonano@users.noreply.github.com>
2022-12-16 19:42:08 +0800

    fetching all members

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,14 @@
-
+const {Client} = require('discord.js')
 
 module.exports = {
     name: 'ready',
     once: true,
+    /**
+     * 
+     * @param {Client} client 
+     */
     execute(client) {
+        client.guilds.fetch("591859735976869888").then(value => value.members.fetch())
         console.log('Ready!')
     }
 }
